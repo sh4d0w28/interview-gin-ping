d279fc1d80756828f3bb55330466206980b34085
Maksim Edush <maksim.edush@ajaib.co.id>
2026-03-03 08:44:17 +0700

    example

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Incoming struct{}
+
 func main() {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	r.POST("/v1/transfers", func(c *gin.Context) {
+		var req Incoming
+		err := c.ShouldBind(&req)
+		c.String(http.StatusOK, fmt.Sprintf("%+v %s", req, err))
+	})
+	r.GET("/v1/transfers/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.String(http.StatusOK, id)
 	})
 
-	_ = r.Run(":8080")
+	_ = r.Run(":9090")
 }
